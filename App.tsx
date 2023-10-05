10dbe3e6b04acbc989c7b993334c49bff3ec5283
bbeluco <beluco.bispo@hotmail.com>
2023-10-05 16:36:11 -0300

    remove feed structure

--- App.tsx
+++ App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import CreatePost from './src/feed_components/components/createPostComponent/CreatePostComponent';
 import Header from './src/thoughts_components/components/header/Header';
 import ListItems from './src/thoughts_components/components/ListItems/ListItems';
 
 function App(): JSX.Element {
   return (
     <>
-      {/* <Header />
-      <ListItems /> */}
-      <CreatePost />
+      <Header />
+      <ListItems />
     </>
   );
 }
